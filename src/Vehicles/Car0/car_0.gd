744a674d77cea815b49af2b2d45e13768324b6cc
4efk <65010619+4efk@users.noreply.github.com>
2024-08-27 11:00:54 +0200

    car work

--- src/Vehicles/Car0/car_0.gd
+++ src/Vehicles/Car0/car_0.gd
@@ -1,8 +1,24 @@
 extends VehicleBody3D
 
 @export var speed = 4000
+@export var breaking_force = 150
+@export var steering_speed: float = 7.5
 
 func _physics_process(delta: float) -> void:
 	engine_force = Input.get_axis("forward", "backward") * speed
-
-	steering = Input.get_axis("right", "left") * PI/8
+	
+	if Input.get_axis("right", "left"):
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, 'steering', Input.get_axis("right", "left") * PI/10, 1/steering_speed)
+	else:
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, 'steering', 0, 1/steering_speed)
+	
+	brake = int(Input.is_action_pressed("brake")) * breaking_force
+	
+	print($WheelFL.get_rpm())
+	
+	#$WheelFL.brake = int(Input.is_action_pressed("brake")) * breaking_force
+	#$WheelFR.brake = int(Input.is_action_pressed("brake")) * breaking_force
+	#$WheelRL.brake = int(Input.is_action_pressed("brake")) * breaking_force
+	#$WheelRR.brake = int(Input.is_action_pressed("brake")) * breaking_force
